f0889c59e5785ef705ca5ab8099082e9ec446d00
Jaehun Park <pjaehun4122@gmail.com>
2021-11-14 15:08:52 -0800

    was able to fetch all users with get request

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,9 +1,13 @@
 const db=require('../data/db-config')
-
+async function getUsers(){
+    const users = await db('users').select('username')
+    return users
+}
 async function register(user){
     const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
     return newUserObject 
 }
 module.exports={
 register,
+getUsers
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,14 +3,19 @@ const express= require('express')
 const Users = require('./users-model')
 const router = express.Router()
 
+router.get('/',(req,res,next)=>{
+    Users.getUsers()
+    .then(users=>{
+        res.status(202).json(users)
+    })
+    .catch(next)
+})
 router.post('/', (req, res, next)=>{
     const UserData=req.body
     Users.register(UserData)
         .then(newUser=>{
             res.status(201).json(newUser)
         })
-        .catch(err=>{
-            next(err)
-        })
+        .catch(next)
 })
 module.exports = router

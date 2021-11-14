039ddfa9f078b3046941c5c570e7bbca04314afd
Jaehun Park <pjaehun4122@gmail.com>
2021-11-14 15:32:30 -0800

    joined table for the shelf router

--- api/shelf/shelf-model.js
+++ api/shelf/shelf-model.js
@@ -1,8 +1,12 @@
 const db=require('../data/db-config')
 async function getAllItems(){
-    const allItems = await db('items').select('item_id','item_name','item_description','item_price')
+    const allItems = await db('items as i')
+        .join('users as u', 'i.user_id','u.user_id')
+        .select('item_id','item_name','item_description','item_price','u.username')
     return allItems
 }
+
+
 module.exports={
     getAllItems
 }
